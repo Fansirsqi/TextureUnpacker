bc72c4122a07e8b222c25cff5fee4f6f98601590
my termux byseven <2595741568@qq.com>
2022-11-08 12:11:21 +0800

    添加兼容模式

--- upc.py
+++ upc.py
@@ -7,11 +7,31 @@ from PIL import Image
 from pathlib import Path
 
 
-def export_image(img, pathname, item):  # sourcery skip: assign-if-exp
+def export_image(img, pathname, item, skey):  # sourcery skip: assign-if-exp
+
+    
     # 去透明后的子图矩形
     x, y, w, h = tuple(map(int, item['frame']))
+
+    #防止宽高为0报错退出程序
+    if w==0:
+        w=1
+    if h==0:
+        h=1
+        
+    key = skey#设置切图模式
     # 子图原始大小
-    size = tuple(map(int, item['sourceSize']))
+    if int(key) == 0:#设置切图模式，0为默认模式，其他为不标准格式)
+        size = tuple(map(int, item['sourceSize']))
+        print(size)
+    else:
+        if item['rotated']:
+            size = tuple([h,w])
+            print(size)
+        else:
+            size = tuple([w,h])
+            print(size)
+           
     # 子图在原始图片中的偏移
     ox, oy, _, _ = tuple(map(int, item['sourceColorRect']))
     # 获取子图左上角，右下角
@@ -19,6 +39,7 @@ def export_image(img, pathname, item):  # sourcery skip: assign-if-exp
         box = (x, y, x + h, y + w)
     else:
         box = (x, y, x + w, y + h)
+
     # 使用原始大小创建图像，全透明
     image = Image.new('RGBA', size, (0, 0, 0, 0))
     # 从图集中裁剪出子图
@@ -27,12 +48,18 @@ def export_image(img, pathname, item):  # sourcery skip: assign-if-exp
     # rotated纹理旋转90度
     if item['rotated']:
         sprite = sprite.transpose(Image.Transpose.ROTATE_90)
-    # 粘贴子图，设置偏移
-    image.paste(sprite, (ox, oy))
+    
+    
+    if int(key) == 0:#设置切图模式，0为默认模式，其他为不标准格式)
+        # 粘贴子图，设置偏移 默认模式
+        image.paste(sprite, (ox, oy))
+    else:
+        # 粘贴子图，设置偏移 非严格模式
+        image.paste(sprite, (0, 0))
+    
     # 保存到文件
-    print(f'写入文件：{pathname} ok!')
     image.save(pathname, 'png')
-
+    print(f'写入文件：{pathname} ok!')
 
 # 获取 frame 参数
 def get_frame(frame):
@@ -59,8 +86,8 @@ def gen_image(file_name):
 
     png = Path(f'{file_name}.png')
     if not os.path.exists(png):
-        print(f'png文件【{plist}】不存在！请检查!!')
-        return (f'[导出]png文件【{plist}】不存在！请检查')
+        print(f'png文件【{png}】不存在！请检查!!')
+        return (f'[导出]png文件【{png}】不存在！请检查')
 
     # 检查导出目录
     export_path = file_name  # 设置为原来的目录fixupdate
@@ -79,9 +106,11 @@ def gen_image(file_name):
 
     # 读取所有小图数据
     frames = lp['frames']
+    print("请输入切图模式，0，标准模式，1，不严格模式")
+    skey = input("请输入:")
     for key in frames:
         item = get_frame(frames[key])
-        export_image(img, os.path.join(export_path, key), item)
+        export_image(img, os.path.join(export_path, key), item, skey)
 
 
 def get_frames_name(file_path):
