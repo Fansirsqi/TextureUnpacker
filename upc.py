b52586636f5bf11d39bdec27de77c02febb3c5b0
fansirsqi <2104898527@qq.com>
2022-11-17 17:57:48 +0800

    格式化代码

--- upc.py
+++ upc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-import plistlib
 import os
+import plistlib
 import sys
 from PIL import Image
 from pathlib import Path
@@ -9,29 +9,28 @@ from pathlib import Path
 
 def export_image(img, pathname, item, skey):  # sourcery skip: assign-if-exp
 
-    
     # 去透明后的子图矩形
     x, y, w, h = tuple(map(int, item['frame']))
 
-    #防止宽高为0报错退出程序
-    if w==0:
-        w=1
-    if h==0:
-        h=1
-        
-    key = skey#设置切图模式
+    # 防止宽高为0报错退出程序
+    if w == 0:
+        w = 1
+    if h == 0:
+        h = 1
+
+    key = skey  # 设置切图模式
     # 子图原始大小
-    if int(key) == 0:#设置切图模式，0为默认模式，其他为不标准格式)
+    if int(key) == 0:  # 设置切图模式，0为默认模式，其他为不标准格式)
         size = tuple(map(int, item['sourceSize']))
         print(size)
     else:
         if item['rotated']:
-            size = tuple([h,w])
+            size = tuple([h, w])
             print(size)
         else:
-            size = tuple([w,h])
+            size = tuple([w, h])
             print(size)
-           
+
     # 子图在原始图片中的偏移
     ox, oy, _, _ = tuple(map(int, item['sourceColorRect']))
     # 获取子图左上角，右下角
@@ -48,18 +47,18 @@ def export_image(img, pathname, item, skey):  # sourcery skip: assign-if-exp
     # rotated纹理旋转90度
     if item['rotated']:
         sprite = sprite.transpose(Image.Transpose.ROTATE_90)
-    
-    
-    if int(key) == 0:#设置切图模式，0为默认模式，其他为不标准格式)
+
+    if int(key) == 0:  # 设置切图模式，0为默认模式，其他为不标准格式)
         # 粘贴子图，设置偏移 默认模式
         image.paste(sprite, (ox, oy))
     else:
         # 粘贴子图，设置偏移 非严格模式
         image.paste(sprite, (0, 0))
-    
+
     # 保存到文件
     image.save(pathname, 'png')
     print(f'写入文件：{pathname} ok!')
+
 
 # 获取 frame 参数
 def get_frame(frame):
@@ -82,12 +81,12 @@ def gen_image(file_name):
     plist = Path(f'{file_name}.plist')
     if not os.path.exists(plist):
         print(f'plist文件【{plist}】不存在！请检查!!')
-        return (f'[导出]plist文件【{plist}】不存在！请检查')
+        return f'[导出]plist文件【{plist}】不存在！请检查'
 
     png = Path(f'{file_name}.png')
     if not os.path.exists(png):
         print(f'png文件【{png}】不存在！请检查!!')
-        return (f'[导出]png文件【{png}】不存在！请检查')
+        return f'[导出]png文件【{png}】不存在！请检查'
 
     # 检查导出目录
     export_path = file_name  # 设置为原来的目录fixupdate
@@ -116,7 +115,7 @@ def gen_image(file_name):
 def get_frames_name(file_path):
     plist = Path(f'{file_path}.plist')
     if not os.path.exists(plist):
-        return (f'[code] plist文件【{plist}】不存在！请检查')
+        return f'[code] plist文件【{plist}】不存在！请检查'
     # im = Path(r'C:\Users\admin\Documents\WeChat Files\wxid_6ri1myvcfaw222\FileStorage\File\2022-10\work\Choji.png')
     lp = plistlib.load(open(plist, 'rb'))
     # img = Image.open(f'{im}')
@@ -124,14 +123,13 @@ def get_frames_name(file_path):
     # # print(frames)
     list_code = ''
     for i in frames:
-        list_code = (f'{list_code}<frameName>{i}</frameName>\n')
+        list_code = f'{list_code}<frameName>{i}</frameName>\n'
     return list_code
 
 
 def get_frame_xy(frame):
-    result = {}
-    result['frame'] = frame['frame'].replace(
-        '}', '').replace('{', '').split(',')
+    result = {'frame': frame['frame'].replace(
+        '}', '').replace('{', '').split(',')}
     # print(type(result['frame']))
     x = result['frame'][0]
     y = result['frame'][1]
